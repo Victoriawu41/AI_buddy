8a4d885e59a41605dc15919415e04a12d1c1e71b
owen <owen2781379@gmail.com>
2025-01-21 15:32:04 -0500

    [INITIAL] create react & install packages

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Hompage from './pages/Hompage'
+
+function App() {
+
+  return (
+    <>
+    <Hompage />
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/Hompage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hompage = () => {
+  return (
+    <div>
+        Meet your AI buddy
+    </div>
+  )
+}
+
+export default Hompage
