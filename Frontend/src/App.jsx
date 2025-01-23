667bd8b75475b3eb692f5e11e15035e8075d86d0
owen <owen2781379@gmail.com>
2025-01-23 17:00:49 -0500

    [UPDATE] Chat UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import HomePage from './pages/HomePage'
 import Chat from './pages/Chat'
