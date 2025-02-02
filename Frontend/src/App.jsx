2977ffbfbbcf70d518f2fb17bd2c403004fde2f2
AlanCh44 <alan.1204.chan@gmail.com>
2025-02-01 20:46:36 -0500

    commiting files related to frontend of register/login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import HomePage from './pages/HomePage'
-import Chat from './pages/Chat'
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import Chat from "./pages/Chat";
 function App() {
-
   return (
-    <BrowserRouter >
-    <Routes>
-      <Route path='/home' element={<HomePage />}></Route>
-      <Route path='/chat' element={<Chat />}></Route>
-    </Routes>
-    </BrowserRouter >
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Default Route - Redirect to login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/home" element={<HomePage />}></Route>
+        <Route path="/chat" element={<Chat />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
